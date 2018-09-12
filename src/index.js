8cfc2d0b3fa3fa61ee991d6b6ec84c23d55174e2
kharuk <frends.nastya@gmail.com>
2018-09-12 11:58:44 +0300

    update task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+    let tempArray = equation.split(' ');
+    let a = Number(tempArray[0]);
+    let b = Number(tempArray[3] + tempArray[4]);
+    let c = Number(tempArray[7] + tempArray[8]);
+    let discriminant = Math.pow(b,2) - 4 * a * c;
+    let firstRoot, secondRoot;
+    if (discriminant > 0){
+        firstRoot = Math.round((-1) * b + Math.sqrt(discriminant)) / (2 * a);
+        secondRoot = Math.round((-1) * b - Math.sqrt(discriminant)) / (2 * a);
+    } else if (discriminant == 0){
+        firstRoot = secondRoot = Math.round((-1) * b / (2 * a));
+    }
+    return [firstRoot, secondRoot].sort((a,b) => a - b);
 }
